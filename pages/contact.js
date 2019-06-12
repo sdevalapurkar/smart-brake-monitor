d533b4ab206ca64577dd40aaf70fedd6cfe9e84d
Arshi Annafi <arshiannafi@gmail.com>
2019-06-12 16:25:42 -0700

    Fix copy-paste error. Index to Contact.

--- pages/contact.js
+++ pages/contact.js
@@ -7,7 +7,8 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 
-class Index extends Component {
+class Contact extends Component {
+
     render() {
         return (<div>
             <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"/>
@@ -47,4 +48,4 @@ class Index extends Component {
     }
 }
 
-export default Index;
+export default Contact;
