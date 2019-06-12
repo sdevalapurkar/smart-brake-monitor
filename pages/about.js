5c95fe8d866e45fd82494d27a0e6f97e8741dde6
Eli Brooks <elibrks@gmail.com>
2019-06-12 14:31:13 -0700

    added about page

---
+++ pages/about.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import NavbarBootstrap from '../components/NavbarBootstrap';
+import Cards from '../components/Cards';
+
+class About extends Component {
+    render() {
+        return (
+            <div>
+                <p>
+                  hello
+                </p>
+            </div>
+        )
+    }
+}
+
+export default About;
