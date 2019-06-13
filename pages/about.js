ddf6a2b539846170178d907855171be5b01c9aab
Eli Brooks <elibrks@gmail.com>
2019-06-12 19:15:58 -0700

    showing a pic, jumbo text

--- pages/about.js
+++ pages/about.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react';
 import NavbarBootstrap from '../components/NavbarBootstrap';
 import Cards from '../components/Cards';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Image from 'react-bootstrap/Image';
 
 class About extends Component {
     render() {
         return (
-            <div>
+          <div>
+              <link
+                  rel="stylesheet"
+                  href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+                  integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+                  crossorigin="anonymous"
+              />
+              <NavbarBootstrap
+                  isAuthenticated={false}
+              />
+              <Jumbotron fluid>
+                <h1>About</h1>
                 <p>
-                  hello
+                  We are a hella group of codebois bringing the craziest lifesaving new brakes to the VEHICLE market
                 </p>
-            </div>
+                <p>
+                    <Image
+                    src={require('../img/logo.png')}
+                    width="40"
+                    height="40"
+                    fluid />
+                </p>
+
+              </Jumbotron>
+
+          </div>
+
         )
     }
 }
