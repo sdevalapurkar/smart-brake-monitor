72e4f29ad7823de15a528a940ccf03d21931842d
Arshi Annafi <arshiannafi@gmail.com>
2019-06-12 17:17:28 -0700

    Add submit functionality

--- pages/contact.js
+++ pages/contact.js
@@ -8,7 +8,6 @@ import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 
 class Contact extends Component {
-
     render() {
         return (<div>
             <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"/>
@@ -21,18 +20,34 @@ class Contact extends Component {
                             <Card.Body>
                                 <Card.Title>Contact Us</Card.Title>
                                 <Card.Text>
-                                    <Form>
+                                    <Form action="https://formspree.io/brakessupreme@gmail.com" method="POST" >
                                         <Form.Group controlId="nameInput">
                                             <Form.Label>Name</Form.Label>
-                                            <Form.Control type="text" placeholder="John Smith"/>
+                                            <Form.Control
+                                                name="name"
+                                                type="text"
+                                                placeholder="John Smith"
+                                                required
+                                            />
                                         </Form.Group>
                                         <Form.Group controlId="emailInput">
                                             <Form.Label>Email address</Form.Label>
-                                            <Form.Control type="email" placeholder="john.smith@mail.me"/>
+                                            <Form.Control
+                                                name="email"
+                                                type="email"
+                                                placeholder="john.smith@mail.me"
+                                                required
+                                            />
                                         </Form.Group>
                                         <Form.Group controlId="messageTextarea">
                                             <Form.Label>Message</Form.Label>
-                                            <Form.Control as="textarea" rows="5" placeholder="Go ahead, we're listening..."/>
+                                            <Form.Control
+                                                name="message"
+                                                as="textarea"
+                                                rows="5"
+                                                placeholder="Go ahead, we're listening..."
+                                                required
+                                            />
                                         </Form.Group>
                                         <Button variant="outline-info" type="submit">
                                             Submit
