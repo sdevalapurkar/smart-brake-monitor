d476092b9e38ee9db4ba27e15023c5d1eb665abf
Eli Brooks <elibrks@gmail.com>
2019-06-13 16:24:23 -0700

    added about text and arranged profiles

--- pages/about.js
+++ pages/about.js
@@ -3,10 +3,15 @@ import NavbarBootstrap from '../components/NavbarBootstrap';
 import Cards from '../components/Cards';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Image from 'react-bootstrap/Image';
+import Figure from 'react-bootstrap/Figure';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
 class About extends Component {
     render() {
         return (
+
           <div>
               <link
                   rel="stylesheet"
@@ -17,20 +22,101 @@ class About extends Component {
               <NavbarBootstrap
                   isAuthenticated={false}
               />
-              <Jumbotron fluid>
-                <h1>About</h1>
-                <p>
-                  We are a hella group of codebois bringing the craziest lifesaving new brakes to the VEHICLE market
-                </p>
-                <p>
-                    <Image
-                    src={require('../img/logo.png')}
-                    width="40"
-                    height="40"
-                    fluid />
-                </p>
 
-              </Jumbotron>
+              <Row className="justify-content-md-center">
+                <h1>About Us</h1>
+              </Row>
+              <Row className="justify-content-md-center">
+                <h6>
+                  We are University of Victoria Engineering students.<br />
+                  Brakes Supreme was created as part of our capstone project course: ECE/SENG 499.<br />
+                  Our group was formed in May 2019<br />
+                </h6>
+              </Row>
+              <Row>
+                <Col md="auto">
+                  <Figure>
+                    <Figure.Image
+                      width={110}
+                      height={118}
+                      alt="110x118"
+                      src={require('../img/logo.png')}
+                    />
+                    <Figure.Caption>
+                      Adam - software
+                    </Figure.Caption>
+                  </Figure>
+                </Col>
+                <Col md="auto">
+                  <Figure>
+                    <Figure.Image
+                      width={110}
+                      height={118}
+                      alt="110x118"
+                      src={require('../img/logo.png')}
+                    />
+                    <Figure.Caption>
+                      Arshi - software
+                    </Figure.Caption>
+                  </Figure>
+                </Col>
+              </Row>
+              <Row>
+                <Col md="auto">
+                  <Figure>
+                    <Figure.Image
+                      width={110}
+                      height={118}
+                      alt="110x118"
+                      src={require('../img/logo.png')}
+                    />
+                    <Figure.Caption>
+                      Shreyas - software
+                    </Figure.Caption>
+                  </Figure>
+                </Col>
+                <Col md="auto">
+                  <Figure>
+                    <Figure.Image
+                      width={110}
+                      height={118}
+                      alt="110x118"
+                      src={require('../img/logo.png')}
+                    />
+                    <Figure.Caption>
+                      Eli - soft/hardware
+                    </Figure.Caption>
+                  </Figure>
+                </Col>
+              </Row>
+              <Row>
+                <Col md="auto">
+                  <Figure>
+                    <Figure.Image
+                      width={110}
+                      height={118}
+                      alt="110x118"
+                      src={require('../img/logo.png')}
+                    />
+                    <Figure.Caption>
+                      Sajan - hardware
+                    </Figure.Caption>
+                  </Figure>
+                </Col>
+                <Col md="auto">
+                  <Figure>
+                    <Figure.Image
+                      width={110}
+                      height={118}
+                      alt="110x118"
+                      src={require('../img/logo.png')}
+                    />
+                    <Figure.Caption>
+                      Rickus - hardware
+                    </Figure.Caption>
+                  </Figure>
+                </Col>
+              </Row>
 
           </div>
 
