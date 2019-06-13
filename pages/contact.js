a0598692d86080b1e106d0e58c0d15c1374e3bf9
Arshi Annafi <arshiannafi@gmail.com>
2019-06-13 15:07:26 -0700

    Moved div onto next line
    item from code review

--- pages/contact.js
+++ pages/contact.js
@@ -9,57 +9,58 @@ import Container from 'react-bootstrap/Container';
 
 class Contact extends Component {
     render() {
-        return (<div>
-            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"/>
-            <NavbarBootstrap isAuthenticated={false}/>
-
-            <Container className="my-3">
-                <Row className="justify-content-center">
-                    <Col md={6}>
-                        <Card>
-                            <Card.Body>
-                                <Card.Title>Contact Us</Card.Title>
-                                <Card.Text>
-                                    <Form action="https://formspree.io/brakessupreme@gmail.com" method="POST" >
-                                        <Form.Group controlId="nameInput">
-                                            <Form.Label>Name</Form.Label>
-                                            <Form.Control
-                                                name="name"
-                                                type="text"
-                                                placeholder="John Smith"
-                                                required
-                                            />
-                                        </Form.Group>
-                                        <Form.Group controlId="emailInput">
-                                            <Form.Label>Email address</Form.Label>
-                                            <Form.Control
-                                                name="email"
-                                                type="email"
-                                                placeholder="john.smith@mail.me"
-                                                required
-                                            />
-                                        </Form.Group>
-                                        <Form.Group controlId="messageTextarea">
-                                            <Form.Label>Message</Form.Label>
-                                            <Form.Control
-                                                name="message"
-                                                as="textarea"
-                                                rows="5"
-                                                placeholder="Go ahead, we're listening..."
-                                                required
-                                            />
-                                        </Form.Group>
-                                        <Button variant="outline-info" type="submit">
-                                            Submit
-                                        </Button>
-                                    </Form>
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                </Row>
-            </Container>
-        </div>)
+        return (
+            <div>
+                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"/>
+                <NavbarBootstrap isAuthenticated={false}/>
+                <Container className="my-3">
+                    <Row className="justify-content-center">
+                        <Col md={6}>
+                            <Card>
+                                <Card.Body>
+                                    <Card.Title>Contact Us</Card.Title>
+                                    <Card.Text>
+                                        <Form action="https://formspree.io/brakessupreme@gmail.com" method="POST" >
+                                            <Form.Group controlId="nameInput">
+                                                <Form.Label>Name</Form.Label>
+                                                <Form.Control
+                                                    name="name"
+                                                    type="text"
+                                                    placeholder="John Smith"
+                                                    required
+                                                />
+                                            </Form.Group>
+                                            <Form.Group controlId="emailInput">
+                                                <Form.Label>Email address</Form.Label>
+                                                <Form.Control
+                                                    name="email"
+                                                    type="email"
+                                                    placeholder="john.smith@mail.me"
+                                                    required
+                                                />
+                                            </Form.Group>
+                                            <Form.Group controlId="messageTextarea">
+                                                <Form.Label>Message</Form.Label>
+                                                <Form.Control
+                                                    name="message"
+                                                    as="textarea"
+                                                    rows="5"
+                                                    placeholder="Go ahead, we're listening..."
+                                                    required
+                                                />
+                                            </Form.Group>
+                                            <Button variant="outline-info" type="submit">
+                                                Submit
+                                            </Button>
+                                        </Form>
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        );
     }
 }
 
