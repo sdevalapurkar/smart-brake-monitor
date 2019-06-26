8c3118d3cfb2b3fbe647f4662732ada9d8c58771
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2019-06-26 16:25:56 -0700

    pool change

--- node-api-postgres/pool.js
+++ node-api-postgres/pool.js
@@ -1,10 +1,10 @@
 const Pool = require('pg').Pool;
 const pool = new Pool({
     user: 'me',
-    host: 'localhost',
+    host: 'https://smart-brake-monitor-server.herokuapp.com',
     database: 'brakessupreme',
     password: 'password',
-    port: 5432,
+    port: process.env.PORT || 5432,
 });
 
 module.exports = pool;
