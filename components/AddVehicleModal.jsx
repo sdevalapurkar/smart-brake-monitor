2509d89ec6ab0b7f682d41bb2cd11743f19b0b45
Arshi Annafi <arshiannafi@gmail.com>
2019-07-04 14:57:30 -0700

    Update the modal with correct UI elements

--- components/AddVehicleModal.jsx
+++ components/AddVehicleModal.jsx
@@ -1,9 +1,24 @@
 import React, {Component} from 'react';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
 
 class AddVehicleModal extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            carName: '',
+            arduinoID: ''
+        };
+    }
+
     render() {
+        const { carName, arduinoID } = this.state;
+
         return (
             <div>
                 <Modal
@@ -12,17 +27,44 @@ class AddVehicleModal extends Component {
                     aria-labelledby="add-vehicle-modal-title"
                     centered
                 >
-                <Modal.Header closeButton>
-                    <Modal.Title id="add-vehicle-modal-title">
-                        Title
-                    </Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    Hello world
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button onClick={this.props.onHide}>Close</Button>
-                </Modal.Footer>
+                    <Modal.Header closeButton>
+                        <Modal.Title id="add-vehicle-modal-title">
+                            Add a New Vehicle
+                        </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Form>
+                            <Form.Group controlId="todo">
+                                <Form.Label>Car Name</Form.Label>
+                                <Form.Control
+                                    name="carName"
+                                    type="text"
+                                    placeholder="Red Tesla Model X"
+                                    value={carName}
+                                    onChange={evt => this.setState({ carName: evt.target.value })}
+                                    required
+                                />
+                            </Form.Group>
+                            <Form.Group controlId="todo">
+                                <Form.Label>Hardware ID</Form.Label>
+                                <Form.Control
+                                    name="carName"
+                                    type="text"
+                                    placeholder="0000-0000-0000-0000"
+                                    value={arduinoID}
+                                    onChange={evt => this.setState({ arduinoID: evt.target.value })}
+                                    required
+                                />
+                            </Form.Group>
+                        </Form>
+                        <Row className="text-center mt-4">
+                            <Col>
+                                <Button
+                                    className="mr-3"variant="outline-danger" onClick={this.props.onHide}>Cancel</Button>
+                                <Button variant="outline-success">Add</Button>
+                            </Col>
+                        </Row>
+                    </Modal.Body>
                 </Modal>
             </div>
         );
