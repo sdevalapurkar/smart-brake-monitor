c51e84963f1f94fb065d95220c886c1ae347f1c2
Arshi Annafi <arshiannafi@gmail.com>
2019-07-10 18:31:52 -0700

    Fix crossOrigin error in console

--- pages/contact.js
+++ pages/contact.js
@@ -43,7 +43,7 @@ class Contact extends Component {
                     rel="stylesheet"
                     href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
                     integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-                    crossorigin="anonymous"
+                    crossOrigin="anonymous"
                 />
 
                 <NavbarBootstrap isAuthenticated={isAuthenticated} name={name} />
--- pages/ourteam.js
+++ pages/ourteam.js
@@ -40,7 +40,7 @@ class Ourteam extends Component {
                     rel="stylesheet"
                     href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
                     integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-                    crossorigin="anonymous"
+                    crossOrigin="anonymous"
                 />
 
                 <NavbarBootstrap isAuthenticated={isAuthenticated} name={name} />
