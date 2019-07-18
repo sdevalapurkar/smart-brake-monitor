56b8722fcf1e43a4070f3d10ad7067ce602ad989
Eli Brooks <elibrks@gmail.com>
2019-07-18 16:03:46 -0700

    freno page adding info

--- pages/freno.js
+++ pages/freno.js
@@ -7,7 +7,9 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Background from '../img/background.jpeg';
+import Image from 'react-bootstrap/Image';
 import axios from 'axios';
+import './index.css';
 
 const host = 'http://localhost';
 const port = 3001;
@@ -38,55 +40,70 @@ class Freno extends Component {
         const { isAuthenticated, name } = this.state;
 
         return (
-            <div style={{ backgroundImage: `url(${Background})`, maxWidth: '100%', height: '100%' }}>
+            <div style={{ maxWidth: '100%', height: '100%' }}>
                 <link
                     rel="stylesheet"
                     href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
                     integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-                    crossorigin="anonymous"
+                    crossOrigin="anonymous"
                 />
-
                 <NavbarBootstrap isAuthenticated={isAuthenticated} name={name} />
 
                 <Container className="my-3">
+
                     <Row className="justify-content-center">
                         <Col className="text-center">
                             <h1>
                                 FRENO
                             </h1>
+                            <h1>
+                                The Smart Brake Monitor
+                            </h1>
+
+                            <Image src={require('../img/freno.jpg')} fluid/>
+                        </Col>
+                    </Row>
+                </Container>
+
+                <Container className="my-3">
+                    <Row className="justify-content-center">
+                        <Col md={8}>
+                            <h2>
+                                Immediate deceleration dependant brake lighting. Constant brake
+                                data tracking and performace grading. Safer and more efficient driving.
+                                No installation required.
+                            </h2>
+                            <p>
+                                watch the demo
+                            </p>
+                            <Image src={require('../img/freno.jpg')} fluid/>
                         </Col>
                     </Row>
                 </Container>
                 <Container className="my-3">
                     <Row className="justify-content-center">
-                        <Col md={8}>
+                        <Col>
+                            <h1>
+                                Installation
+                            </h1><hr />
                             <p>
-                            &nbsp;&nbsp;&nbsp;When driving, it is extremely important to stay focused and have the ability to
-                            respond to situations in a timely manner. Not being able to react to situations effectively
-                            can lead to crashes and collisions. It has been shown that rear-end crashes are the most
-                            frequent type of vehicle collision, accounting for 29% of all crashes. The current
-                            solution used in vehicles to try and tackle this issue are brake lights.
+                            Freno is easy to install, simply put the electronics into a box or use one of the boxes we
+                            have provided. Lay this box down flat into the trunk of back seat of your vehicle.
+                            Ensure the wire that the LED strip is attached to can reach your rear window
                             </p>
-                            <p>
-                            &nbsp;&nbsp;&nbsp;Brake lights present at the back of vehicles have two states, on or off. By default,
-                            brake lights are switched off and when the brake is engaged they switch on. The purpose
-                            of these brake lights is to indicate vehicle deceleration. Currently, brake lights provide an
-                            indication as to whether or not the brakes of a vehicle have been applied; however, they
-                            do not indicate how suddenly the vehicle is slowing down. A light brake that provides
-                            minor deceleration displays the same warning indicator as a sudden brake with a large
-                            deceleration. Although the default brake light is a good warning indicator that a vehicle is
-                            decelerating, it can be improved as it does not provide information regarding the severity
-                            of the brake being applied.
-                            </p>
-                            <p>
-                            &nbsp;&nbsp;&nbsp;Freno aims to address this issue by providing a spectrum of values to describe
-                            vehicle braking rather than a boolean state. By providing a range of accurate real-time
-                            values, drivers can understand the severity of the brake being applied and make a better
-                            decision with respect to how hard they need to brake themselves. Freno will also track
-                            brake deceleration readings and through a custom web interface, will allow drivers to
-                            track their driving habits with regards to braking and see how it is impacting aspects of
-                            their vehicle such as fuel consumption and brake health.
-                            </p>
+                            <h1>
+                                Specs
+                            </h1><hr/>
+                            <h2>
+                                Dimensions
+                            </h2><hr/>
+                            <h2>
+                                Parts
+                            </h2><hr/>
+                            <h2>
+                                Documentation
+                            </h2><hr/>
+
                         </Col>
                     </Row>
                 </Container>
