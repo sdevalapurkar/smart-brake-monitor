6ac380a64e82ec9de077d2d05acdf21c3fb1d244
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2019-07-19 16:33:46 -0700

    add mock data and basic script parsing start

---
+++ scripts/parsebrakingdata.py
@@ -0,0 +1,13 @@
+import csv
+
+with open('../data/testbrakingdata.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+
+    for row in csv_reader:
+        if line_count == 0:
+            print(f'Column names are {", ".join(row)}')
+            line_count += 1
+        else:
+            print(f'\t{row[0]} {row[1]} {row[2]} {row[3]} {row[4]} {row[5]} {row[6]}')
+            line_count += 1
