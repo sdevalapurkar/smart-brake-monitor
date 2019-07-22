99474d4a2475e64345602491b3fa4a33d902e4b6
Arshi Annafi <arshiannafi@gmail.com>
2019-07-21 21:40:08 -0700

    Add commit after execute in sql script

--- scripts/db.py
+++ scripts/db.py
@@ -14,5 +14,7 @@ cur = con.cursor()
 cur.execute("create table users(user_id serial, name varchar(40), email varchar(40) primary key, password text not null)")
 cur.execute("create table vehicles(vehicle_id integer primary key, is_activated boolean, vehicle_name varchar(100) not null, vehicle_weight float not null, tire_specs text not null, email varchar(40) references users(email))")
 
+con.commit()
+
 # close the connection
 con.close()
