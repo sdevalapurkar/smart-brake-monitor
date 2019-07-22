d32666a2d5379a12569f44bd759192f19306af9a
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2019-07-22 12:52:05 -0700

    populating braking data table with values

---
+++ scripts/populatebrakingtable.py
@@ -0,0 +1,39 @@
+import csv
+import psycopg2
+from datetime import datetime
+
+txt = input("Please enter your Freno ID: ")
+freno_id = int(txt)
+
+# connect to the database
+con = psycopg2.connect(
+    user = "me",
+    host = "localhost",
+    database = "brakessupreme",
+    password = "password",
+    port = 5432
+)
+
+# cursor
+cur = con.cursor()
+
+with open('../data/parsed_braking_data.csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+
+    for row in csv_reader:
+        relative_time_count = float(row[0]) * 5
+        dec_x = float(row[1])
+        dec_y = float(row[2])
+        dec_z = float(row[3])
+        gy_x = float(row[4])
+        gy_y = float(row[5])
+        gy_z = float(row[6])
+        drive_date = datetime.today().strftime("%Y-%m-%d")
+
+        cur.execute("INSERT INTO brakingdata(vehicle_id, dec_x, dec_y, dec_z, gy_x, gy_y, gy_z, drive_date, relative_time_count) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)", (freno_id, dec_x, dec_y, dec_z, gy_x, gy_y, gy_z, drive_date, relative_time_count))
+
+# commit the transaction
+con.commit()
+
+# close the connection
+con.close()
