bf0fecc699908e4f308abe7f0a3728e2a844572b
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2019-07-22 13:14:29 -0700

    add constraints and safety checks

--- scripts/db.py
+++ scripts/db.py
@@ -15,6 +15,7 @@ cur = con.cursor()
 cur.execute("create table users(user_id serial, name varchar(40), email varchar(40) primary key, password text not null)")
 cur.execute("create table vehicles(vehicle_id integer primary key, is_activated boolean, vehicle_name varchar(100) not null, vehicle_weight float not null, tire_specs text not null, email varchar(40) references users(email))")
 cur.execute("create table brakingdata(vehicle_id integer references vehicles(vehicle_id), dec_x float, dec_y float, dec_z float, gy_x float, gy_y float, gy_z float, drive_date date, relative_time_count integer)")
+cur.execute("alter table brakingdata add primary key(vehicle_id, drive_date, relative_time_count)")
 
 # commit the transaction
 con.commit()
--- scripts/populatebrakingtable.py
+++ scripts/populatebrakingtable.py
@@ -2,8 +2,17 @@ import csv
 import psycopg2
 from datetime import datetime
 
-txt = input("Please enter your Freno ID: ")
-freno_id = int(txt)
+failed_flag = False
+
+while True:
+    try:
+        txt = input("Please enter your Freno ID: ")
+        freno_id = int(txt)
+    except ValueError:
+        print("Sorry, please enter a valid Freno ID.")
+        continue
+    else:
+        break
 
 # connect to the database
 con = psycopg2.connect(
@@ -30,10 +39,16 @@ with open('../data/parsed_braking_data.csv') as csv_file:
         gy_z = float(row[6])
         drive_date = datetime.today().strftime("%Y-%m-%d")
 
-        cur.execute("INSERT INTO brakingdata(vehicle_id, dec_x, dec_y, dec_z, gy_x, gy_y, gy_z, drive_date, relative_time_count) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)", (freno_id, dec_x, dec_y, dec_z, gy_x, gy_y, gy_z, drive_date, relative_time_count))
+        try:
+            cur.execute("INSERT INTO brakingdata(vehicle_id, dec_x, dec_y, dec_z, gy_x, gy_y, gy_z, drive_date, relative_time_count) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)", (freno_id, dec_x, dec_y, dec_z, gy_x, gy_y, gy_z, drive_date, relative_time_count))
+        except:
+            failed_flag = True
 
 # commit the transaction
-con.commit()
+if failed_flag is False:
+    con.commit()
+else:
+    print("Sorry there was an error. Please try again")
 
 # close the connection
 con.close()
