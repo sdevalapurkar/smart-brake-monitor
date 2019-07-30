463a08223ccbab39a431380d35866594ff52c397
Eli Brooks <elibrks@gmail.com>
2019-07-29 21:39:46 -0700

    fixed stuff, pr time, still no ref

--- pages/freno.js
+++ pages/freno.js
@@ -49,13 +49,13 @@ class Freno extends Component {
                 <NavbarBootstrap isAuthenticated={isAuthenticated} name={name} />
 
                 <Container className="my-5">
-                    <Card className="mt-5">
+                    <Card className="mt-5 justify-content-center">
                         <Card.Body className="mb-3">
                             <Row className="my-5">
                                 <Col className="text-center">
                                     <h1 className="font-35-bold mb-5 mb-md-0">FRENO</h1>
                                 </Col>
-                                <Col xs={12} md={6}>
+                                <Col xs={12} md={6} className="text-center">
                                     <h3>
                                         Accident If You <b>Don't Know</b><br/>
                                         Prevention If You <b>Freno</b>
@@ -79,11 +79,11 @@ class Freno extends Component {
                                     <h1>
                                         Short Demo
                                     </h1>
-                                    <YouTube videoId='BtLwoNJ6klE' opts = {{height: '390', width: '640'}}/>
+                                    <YouTube videoId='BtLwoNJ6klE' className="freno-video" opts = {{height: '390', width: '640'}} />
                                 </Col>
                             </Row>
                             <Row className="justify-content-center mt-5">
-                                <Col  md={10} className="mb-3 mb-md-0">
+                                <Col md={10} className="mb-3 mb-md-0">
                                     <h1>
                                         How It Works
                                     </h1><hr />
@@ -111,9 +111,9 @@ class Freno extends Component {
                                 </Col>
                             </Row>
                             <Row className="justify-content-center">
-                                <Col md={10} className="mb-3 mb-md-0">
+                                <Col md={18} lg={18} className="mb-3 mb-md-0" >
                                     <CardDeck>
-                                        <Card style={{ width: '18rem', borderRadius: '1em', boxShadow: '0px 5px 32px -16px' }}>
+                                        <Card className="freno-card" style={{ width: '18rem', borderRadius: '1em', boxShadow: '0px 5px 32px -16px' }}>
                                           <Card.Img variant="top" src={require('../img/accelerometer.jpg')} style={{ borderRadius: '1em 1em 0em 0em' }}/>
                                           <Card.Body>
                                             <Card.Title>Accelerometer</Card.Title>
@@ -159,7 +159,7 @@ class Freno extends Component {
                                 </Col>
                             </Row>
                             <Row className="justify-content-center mt-5">
-                                <Col md={10} className="mb-3 mb-md-0" >
+                                <Col md={18} lg={18} className="mb-3 mb-md-0" >
                                     <CardDeck>
                                         <Card style={{ width: '18rem', borderRadius: '1em', boxShadow: '0px 5px 32px -16px' }}>
                                           <Card.Img variant="top" src={require('../img/led_strip.jpg')} style={{ borderRadius: '1em 1em 0em 0em' }}/>
@@ -200,6 +200,20 @@ class Freno extends Component {
                                           </Card.Footer>
                                         </Card>
                                     </CardDeck>
+                                </Col>
+                            </Row>
+                        </Card.Body>
+                    </Card>
+                    <Card className="mt-5 justify-content-center">
+                        <Card.Body className="mb-3">
+                            <Row className="justify-content-center mt-5">
+                                <Col md={10} className="mb-3 mb-md-0">
+                                    <h2>
+                                        References
+                                    </h2><hr />
+                                    <p>
+                                        Github
+                                    </p>
                                 </Col>
                             </Row>
                         </Card.Body>
